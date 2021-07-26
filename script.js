b44ede6ddea17c5e0431c3f823eb41ac2ab6e339
maddenp-etsu <59734882+maddenp-etsu@users.noreply.github.com>
2021-07-26 10:43:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+window.onload = () => {
 let frame = document.createElement("iframe");
 frame.referrerPolicy = "origin";
 frame.src = "https://www.youtube.com/live_chat?v=AMAefQa3cII&embed_domain=maddenp-etsu.github.io" + window.location.hostname;
@@ -6,4 +6,4 @@ frame.frameBorder = "0";
 frame.id = "chat-embed";
 let wrapper = document.getElementById("chat-embed-wrapper");
 wrapper.appendChild(frame);
-})
+}
