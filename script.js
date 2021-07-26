9fc576d0c72199dacfe0cc8e03f9af4d47426542
maddenp-etsu <59734882+maddenp-etsu@users.noreply.github.com>
2021-07-26 14:25:05 -0400

    Update URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
 let frame = document.createElement("iframe");
 frame.referrerPolicy = "origin";
-frame.src = "https://www.youtube.com/live_chat?v=AMAefQa3cII&embed_domain=maddenp-etsu.github.io" + window.location.hostname;
+frame.src = "https://www.youtube.com/live_chat?v=pwv1OYVa1v0&embed_domain=maddenp-etsu.github.io" + window.location.hostname;
 frame.frameBorder = "0";
 frame.id = "chat-embed";
 let wrapper = document.getElementById("chat-embed-wrapper");
